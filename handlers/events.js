ecb19cb82d70048673da1f4b61e0a9864fa941fb
Juan López <juan07alberto@gmail.com>
2020-01-16 10:45:01 -0600

    handlers events addded

---
+++ handlers/events.js
@@ -0,0 +1,24 @@
+const { Event } = require('../ps-models/event')
+const { Course } = require('../ps-models/course')
+const uuid4 = require('uuid/v4')
+const Boom = require('@hapi/boom')
+const Sequelize = require('sequelize')
+
+async function setEvent( { payload }, h){
+    let { uuidCourse } = payload
+    let course, event
+    try{
+        course = await Course.findOne({ where: { uuidCourse } })
+    }catch(error){
+        throw Boom.badImplementation('Something went wrong')
+    }
+    const dataEvent = Object.assign(payload, { uuid : uuid4() })
+    if(course.dataValues != null){
+        try{
+            ok = await Event.create(dataEvent)
+        }catch(error){
+            throw Boom.badImplementation('Something went wrong')
+        }
+    }
+    return h.response({ id: user.uuid, message: 'Successfully created'}).code(201).header('Content-Type', 'application/json')
+}
